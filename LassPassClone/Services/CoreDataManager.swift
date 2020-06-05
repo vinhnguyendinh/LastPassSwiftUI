e2ab8fc12d25c2fbab6998b59ba21ec44542f7c3
Vinh Nguyen <nguyen-dinh-vinh@goodcycle.net>
2020-06-05 19:12:18 +0900

    update core data manager

---
+++ LassPassClone/Services/CoreDataManager.swift
@@ -0,0 +1,9 @@
+//
+//  CoreDataManager.swift
+//  LassPassClone
+//
+//  Created by Vinh Nguyen Dinh on 2020/06/02.
+//  Copyright © 2020 Vinh Nguyen Dinh. All rights reserved.
+//
+
+import Foundation
